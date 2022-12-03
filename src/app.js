31fcfdaaa6d05d58e495e789f0990854da91054a
Aaron Campecino <aaroncampecino@gmail.com>
2022-12-03 11:33:49 +0800

    use non socket

--- src/app.js
+++ src/app.js
@@ -28,12 +28,60 @@ switch (process.env.LOG_LEVEL) {
 }
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: process.env.SLACK_SOCKET_MODE === "true",
+  signingSecret: process.env.SLACK_BOT_SIGNING_SECRET,
+  // token: process.env.SLACK_BOT_TOKEN,
+  clientId: process.env.SLACK_CLIENT_ID,
+  // appToken: process.env.SLACK_APP_TOKEN,
+  // socketMode: process.env.SLACK_SOCKET_MODE === "true",
   scopes: manifest.oauth_config.scopes.bot,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
   logLevel: logLevel,
   customRoutes: customRoutes.customRoutes,
+  stateSecret: "aaron-the-great",
+  installerOptions: {
+    stateVerification: false,
+    // directInstall: true,
+  },
+  installationStore: {
+    storeInstallation: async (installation) => {
+      if (
+        installation.isEnterpriseInstall &&
+        installation.enterprise !== undefined
+      ) {
+        return await orgAuth.saveUserOrgInstall(installation);
+      }
+      if (installation.team !== undefined) {
+        return await workspaceAuth.saveUserWorkspaceInstall(installation);
+      }
+      throw new Error("Failed saving installation data to installationStore");
+    },
+    fetchInstallation: async (installQuery) => {
+      // console.log("installQuery " + installQuery);
+      // console.log(installQuery);
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        return await user.findUser(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        return await user.findUser(installQuery.teamId);
+      }
+      throw new Error("Failed fetching installation");
+    },
+    deleteInstallation: async (installQuery) => {
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        return await user.deleteUser(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        return await user.deleteUser(installQuery.teamId);
+      }
+      throw new Error("Failed to delete installation");
+    },
+  },
 });
 
 registerListeners(app);
